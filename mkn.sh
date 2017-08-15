9c5b4e3cf33a7876ff533c3425f9e9cb4d7dffce
dekken <philip.deegan@gmail.com>
2017-08-15 20:04:36 +0100

    more

--- mkn.sh
+++ mkn.sh
@@ -22,32 +22,30 @@ MKN_CXXR="-O2 -fPIC"
 MKN_CXXR=${CXXFLAGS:-$MKN_CXXR}
 MKN_REPO="$(mkn -G MKN_REPO)"
 VER_BOOST="$(mkn -G org.boost.version)"
-LOC_HWLOC="$(mkn -G hwloc.location)"
+VER_HWLOC="$(mkn -G ompi.hwloc.version)"
 
 THREADS="$(nproc --all)"
 
-[ ! -d "$CWD/v" ] && git clone $GIT_OPT $GIT_URL -b $GIT_BNC v --recursive
+[ ! -d "$CWD/hpx" ] && git clone $GIT_OPT $GIT_URL -b $GIT_BNC hpx --recursive
 
 KLOG=3 mkn clean build -dtSa "${MKN_CXXR[@]}"
 
-mkdir -p $CWD/v/build
-pushd $CWD/v/build
+mkdir -p $CWD/hpx/build
 
+pushd $CWD/hpx/build
 read -r -d '' CMAKE <<- EOM || echo "running cmake"
     cmake -DBOOST_ROOT=$MKN_REPO/org/boost/$VER_BOOST/b 
-          -DHWLOC_ROOT=$LOC_HWLOC 
+          -DHWLOC_ROOT=$MKN_REPO/ompi/hwloc/$VER_HWLOC/inst
           -DHPX_WITH_MALLOC=$HPX_WITH_MALLOC 
           -DCMAKE_INSTALL_PREFIX=$CWD/inst 
           -DCMAKE_BUILD_TYPE=Release ..
 EOM
-
-echo $CMAKE
 $CMAKE
-
 make -j$THREADS
 make install
-
 popd
 
+echo "Running test"
+$CWD/inst/bin/hello_world
 echo "Finished successfully"
 exit 0
